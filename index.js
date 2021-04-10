4af5d299a17af566c41bf29d6e94c3b1d0756fa0
AndrewCrescencio <68707868+AndrewCrescencio@users.noreply.github.com>
2021-04-10 05:45:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar__links')[0];
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar__links')[0]
 
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle('active');
-});
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const dropdownButton = document.querySelectorAll('.dropdownButton');
+const dropdownMenu = document.querySelectorAll('.footer__links__dropdown__menu');
+
+for (let i = 0; i < dropdownButton.length; i++) {
+    dropdownButton[i].addEventListener("click", function() {
+        dropdownMenu[i].classList.toggle("active");
+    });
+}
