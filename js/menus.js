92b595794c8f5a601b1bfcc462239ff0d7355d92
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-08-09 20:47:43 -0300

    refactor: remove commets

--- js/menus.js
+++ js/menus.js
@@ -21,22 +21,4 @@ export default class Menus {
       });
     }
   }
-  // menusToggle() {
-  //   const toggleButton = document.getElementById("toggle-button");
-  //   const navbarLinks = document.getElementById("navbarLinks");
-  //   toggleButton.addEventListener("click", () => {
-  //     navbarLinks.classList.toggle("active");
-  //   });
-
-  //   const dropdownButton = document.querySelectorAll(".dropdownButton");
-  //   const dropdownMenu = document.querySelectorAll(
-  //     ".footer__links__dropdown__links"
-  //   );
-
-  //   for (let i = 0; i < dropdownButton.length; i++) {
-  //     dropdownButton[i].addEventListener("click", function () {
-  //       dropdownMenu[i].classList.toggle("active");
-  //     });
-  //   }
-  // }
 }
